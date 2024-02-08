64814e5635c36b74e52e457e943f37c2bf7d4e3c
krishnakp17 <krishnakp4765@gmail.com>
2024-02-09 01:19:22 +0530

    viewed teacher

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddTeacher from './components/AddTeacher';
+import ViewTeacher from './components/ViewTeacher';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<AddTeacher/>}/>
+        <Route path='/view' element={<ViewTeacher/>}/>
       </Routes>
       </BrowserRouter>
     </div>
